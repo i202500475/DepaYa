c3a86ac36e3422152ccf03490f7fa45b93c7fbc7
SaraA <saraasenjo9@gmail.com>
2026-07-30 17:46:12 -1200

    Primer commit

---
+++ eureka-server/src/test/java/com/depaya/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depaya.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
